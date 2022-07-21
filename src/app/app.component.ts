463ef43a38b2adbb2639a087f2dfcadde9084b97
André Lesser <andrelesser@web.de>
2022-07-21 15:58:38 +0200

    Added images to slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'andrelesser';
+export class AppComponent implements OnInit {
+  images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg'];
+  currentImage=0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage=false;
+
+      setTimeout(() => {
+        this.showImage=true;
+      },10)
+    },8000)
+  }
 }
